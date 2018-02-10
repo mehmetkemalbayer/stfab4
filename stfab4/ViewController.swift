79b3d54afb3fd2c48222a6cc01f0d4731a5b7626
Mehmet Kemal Bayer <kemal.bayer@gmail.com>
2018-02-10 18:00:34 +0300

    Initial Commit

---
+++ stfab4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stfab4
+//
+//  Created by MKB on 10/02/2018.
+//  Copyright © 2018 MKB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
